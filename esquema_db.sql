14306d4da11c9bcc3bdf1a9a9b701c0079d67539
Matias <vazquez.matias.e@gmail.com>
2025-11-04 16:38:37 -0300

    Se corrige esquema_db.sql y se agrega datos_db.sql

--- esquema_db.sql
+++ esquema_db.sql
@@ -1,35 +1,16 @@
-DROP SCHEMA IF EXISTS db_pedido_envio;
-CREATE SCHEMA db_pedido_envio;
-USE db_pedido_envio;
+CREATE DATABASE IF NOT EXISTS pedido_envio_db;
+
+DROP SCHEMA IF EXISTS pedido_envio;
+CREATE SCHEMA pedido_envio;
+USE pedido_envio;
 
 DROP TABLE IF EXISTS PEDIDO;
 DROP TABLE IF EXISTS ENVIO;
 
-USE db_pedido_envio;
-
-CREATE TABLE PEDIDO (
-    id LONG AUTO_INCREMENT PRIMARY KEY,
-    eliminado BOOL,
-    numero VARCHAR(20) NOT NULL,
-    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    cliente_nombre VARCHAR(120) NOT NULL,
-    total DECIMAL(12,2) NOT NULL,
-    estado VARCHAR(10) NOT NULL DEFAULT 'Nuevo',
-    envio_id LONG NOT NULL,
-    CONSTRAINT fk_pedido_envio FOREIGN KEY (envio_id) 
-        REFERENCES ENVIO(id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-	CONSTRAINT uk_pedido_numero UNIQUE (numero),
-    CONSTRAINT uk_pedido_id UNIQUE (id),
-    CONSTRAINT chk_pedido_estado CHECK (
-        estado_pedido IN ('Nuevo', 'Facturado', 'Enviado')
-    ),
-    CONSTRAINT chk_pedido_total CHECK (total >= 0)
-);
+USE pedido_envio;
 
 CREATE TABLE ENVIO (
-    id LONG AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     eliminado BOOL,
     tracking VARCHAR(40),
     empresa VARCHAR(10) NOT NULL,
@@ -43,6 +24,28 @@ CREATE TABLE ENVIO (
     CONSTRAINT chk_envio_estado CHECK (
         estado IN ('En Preparacion', 'En Transito', 'Entregado')
     ),
-    CONSTRAINT chk_envio_costo CHECK (costo_envio >= 0),
+    CONSTRAINT chk_envio_costo CHECK (costo >= 0),
     CONSTRAINT chk_envio_fechas CHECK (fecha_entrega >= fecha_despacho)
+);
+
+
+CREATE TABLE PEDIDO (
+    id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    eliminado BOOL,
+    numero VARCHAR(20) NOT NULL,
+    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    cliente_nombre VARCHAR(120) NOT NULL,
+    total DECIMAL(12,2) NOT NULL,
+    estado VARCHAR(10) NOT NULL DEFAULT 'Nuevo',
+    envio_id INT NOT NULL UNIQUE,
+    CONSTRAINT fk_pedido_envio FOREIGN KEY (envio_id) 
+        REFERENCES ENVIO(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+	CONSTRAINT uk_pedido_numero UNIQUE (numero),
+    CONSTRAINT uk_pedido_id UNIQUE (id),
+    CONSTRAINT chk_pedido_estado CHECK (
+        estado IN ('Nuevo', 'Facturado', 'Enviado')
+    ),
+    CONSTRAINT chk_pedido_total CHECK (total >= 0)
 );
